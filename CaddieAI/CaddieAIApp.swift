54b10f52f34bb515da25f46aebf12f00f6b80df1
Ash Patel <apatel713@gmail.com>
2026-03-18 14:16:54 -0600

    Initial Commit

---
+++ CaddieAI/CaddieAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaddieAIApp.swift
+//  CaddieAI
+//
+//  Created by Aashish Patel on 3/18/26.
+//
+
+import SwiftUI
+
+@main
+struct CaddieAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
